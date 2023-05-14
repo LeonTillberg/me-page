4de09cf3749e74050a9f01205e005c5b5438a7ac
LeonTillberg <leontm@live.se>
2023-05-14 19:48:33 +0200

    bytat ut majoriteten av filler texten

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function opentab(tabname) {
     event.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add('active-tab');
 }
+
+const sidemenu = document.getElementById('sidemenu');
+
+function openmenu(){
+    sidemenu.style.right = '0';
+}
+
+function closemenu(){
+    sidemenu.style.right = '-200px';
+}
